6b3ba24ac4062473d7708fcb0101a4ed4bac2a82
Mack <mackschmaltz@gmail.com>
2021-04-07 20:43:10 -0600

    Added Translator Key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // config for Wit API calls
-// put here so that the API key can be hidden and not publicly faced
+// put here so that the API keys can be hidden and not publicly faced
 module.exports = {
-    key: 'CDXDELMNRPVNCMWN3J7LX73SYYFQS32J',
+    witKey: 'CDXDELMNRPVNCMWN3J7LX73SYYFQS32J',
+    translatorKey: '3d7e587b4fac49069cc4dce469c276f5'
 }
