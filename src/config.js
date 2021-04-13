5e8dbaba807895503fdc8e160045bdcaca1d44bb
Mack <mackschmaltz@gmail.com>
2021-04-13 16:46:21 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config for Wit API calls
 // put here so that the API keys can be hidden and not publicly faced
 module.exports = {
-    witKey: 'CDXDELMNRPVNCMWN3J7LX73SYYFQS32J',
+    witKey: 'ENTER KEY HERE',
     translatorKey: '3d7e587b4fac49069cc4dce469c276f5'
 }
